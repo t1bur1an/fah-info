fb2da13bac340c765f97e2f234a1ba9611273b7f
t1bur1an <t1bur1an@gmail.com>
2023-05-27 20:37:27 +0300

    Update main.go

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strconv"
+	"strings"
+	"time"
 )
 
 type Basic struct {
@@ -41,15 +44,15 @@ type Slot struct {
 
 type Response struct {
 	Path  string
-	Value map[string]interface{}
+	Value []Slot
 }
 
 type mainResponse interface {
 }
 
-func startSession(ipAddress string) (error, string) {
+func sendRequest(ipAddress string, url string) (error, string) {
 	// Create a new HTTP request
-	req, err := http.NewRequest(http.MethodPut, "http://"+ipAddress+":7396/api/session", nil)
+	req, err := http.NewRequest(http.MethodPut, url, nil)
 	if err != nil {
 		fmt.Println("Error creating HTTP request:", err)
 		return err, ""
@@ -89,142 +92,49 @@ func startSession(ipAddress string) (error, string) {
 	return nil, string(body)
 }
 
+func startSession(ipAddress string) (error, string) {
+	err, data := sendRequest(ipAddress, "http://"+ipAddress+":7396/api/session")
+	return err, data
+}
+
 func fahConfigured(ipAddress string, token string) {
-	// Create a new HTTP request
-	req, err := http.NewRequest(http.MethodPut, "http://"+ipAddress+":7396/api/configured?sid="+token+"&_=1685187908670", nil)
+	err, _ := sendRequest(ipAddress, "http://"+ipAddress+":7396/api/configured?sid="+token+"&_=1685187908670")
 	if err != nil {
-		fmt.Println("Error creating HTTP request:", err)
+		return
 	}
-
-	// Set the headers
-	req.Header.Set("sec-ch-ua", `"(Not(A:Brand";v="8", "Chromium";v="100"`)
-	req.Header.Set("Accept", "*/*")
-	req.Header.Set("X-Requested-With", "XMLHttpRequest")
-	req.Header.Set("sec-ch-ua-mobile", "?0")
-	req.Header.Set("User-Agent", `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36`)
-	req.Header.Set("sec-ch-ua-platform", `"macOS"`)
-	req.Header.Set("Origin", "http://"+ipAddress+":7396")
-	req.Header.Set("Sec-Fetch-Site", "same-origin")
-	req.Header.Set("Sec-Fetch-Mode", "cors")
-	req.Header.Set("Sec-Fetch-Dest", "empty")
-	req.Header.Set("Referer", "http://"+ipAddress+":7396/?nocache=0.4642702738328919")
-	req.Header.Set("Accept-Encoding", "gzip, deflate")
-	req.Header.Set("Accept-Language", "en-GB,en-US;q=0.9,en;q=0.8")
-	req.Header.Set("Connection", "close")
-
-	// Send the HTTP request
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Println("Error sending HTTP request:", err)
-	}
-	defer resp.Body.Close()
 }
 
 func fahSetApi(ipAddress string, token string) {
-	// Create a new HTTP request
-	req, err := http.NewRequest(http.MethodPut, "http://"+ipAddress+":7396/api/updates/set?sid="+token+"&update_id=1&update_rate=1&update_path=%2Fapi%2Fslots&_=1685187908669", nil)
+	err, _ := sendRequest(ipAddress, "http://"+ipAddress+":7396/api/updates/set?sid="+token+"&update_id=1&update_rate=1&update_path=%2Fapi%2Fslots&_=1685187908669")
 	if err != nil {
-		fmt.Println("Error creating HTTP request:", err)
+		return
 	}
-
-	// Set the headers
-	req.Header.Set("sec-ch-ua", `"(Not(A:Brand";v="8", "Chromium";v="100"`)
-	req.Header.Set("Accept", "*/*")
-	req.Header.Set("X-Requested-With", "XMLHttpRequest")
-	req.Header.Set("sec-ch-ua-mobile", "?0")
-	req.Header.Set("User-Agent", `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36`)
-	req.Header.Set("sec-ch-ua-platform", `"macOS"`)
-	req.Header.Set("Origin", "http://"+ipAddress+":7396")
-	req.Header.Set("Sec-Fetch-Site", "same-origin")
-	req.Header.Set("Sec-Fetch-Mode", "cors")
-	req.Header.Set("Sec-Fetch-Dest", "empty")
-	req.Header.Set("Referer", "http://"+ipAddress+":7396/?nocache=0.4642702738328919")
-	req.Header.Set("Accept-Encoding", "gzip, deflate")
-	req.Header.Set("Accept-Language", "en-GB,en-US;q=0.9,en;q=0.8")
-	req.Header.Set("Connection", "close")
-
-	// Send the HTTP request
-	client := &http.Client{}
-	resp, err := client.Do(req)
+	err, _ = sendRequest(ipAddress, "http://"+ipAddress+":7396/api/updates/set?sid="+token+"&update_id=0&update_rate=1&update_path=%2Fapi%2Fbasic&_=1685187908670")
 	if err != nil {
-		fmt.Println("Error sending HTTP request:", err)
+		return
 	}
-	defer resp.Body.Close()
-
-	// Create a new HTTP request
-	req, err = http.NewRequest(http.MethodPut, "http://"+ipAddress+":7396/api/updates/set?sid="+token+"&update_id=0&update_rate=1&update_path=%2Fapi%2Fbasic&_=1685187908670", nil)
-	if err != nil {
-		fmt.Println("Error creating HTTP request:", err)
-	}
-
-	// Set the headers
-	req.Header.Set("sec-ch-ua", `"(Not(A:Brand";v="8", "Chromium";v="100"`)
-	req.Header.Set("Accept", "*/*")
-	req.Header.Set("X-Requested-With", "XMLHttpRequest")
-	req.Header.Set("sec-ch-ua-mobile", "?0")
-	req.Header.Set("User-Agent", `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36`)
-	req.Header.Set("sec-ch-ua-platform", `"macOS"`)
-	req.Header.Set("Origin", "http://"+ipAddress+":7396")
-	req.Header.Set("Sec-Fetch-Site", "same-origin")
-	req.Header.Set("Sec-Fetch-Mode", "cors")
-	req.Header.Set("Sec-Fetch-Dest", "empty")
-	req.Header.Set("Referer", "http://"+ipAddress+":7396/?nocache=0.4642702738328919")
-	req.Header.Set("Accept-Encoding", "gzip, deflate")
-	req.Header.Set("Accept-Language", "en-GB,en-US;q=0.9,en;q=0.8")
-	req.Header.Set("Connection", "close")
-
-	// Send the HTTP request
-	client = &http.Client{}
-	resp, err = client.Do(req)
-	if err != nil {
-		fmt.Println("Error sending HTTP request:", err)
-	}
-	defer resp.Body.Close()
 }
 
-func getStatus(ipAddress string, token string) string {
-	// Create a new HTTP request
-	req, err := http.NewRequest(http.MethodPut, "http://"+ipAddress+":7396/api/updates?sid="+token+"&_=1685187908670", nil)
+func getStatus(ipAddress string, token string) (error, []Slot) {
+	err, body := sendRequest(ipAddress, "http://"+ipAddress+":7396/api/slots?sid="+token+"&_=1685187908670")
 	if err != nil {
-		fmt.Println("Error creating HTTP request:", err)
+		return err, nil
 	}
 
-	// Set the headers
-	req.Header.Set("sec-ch-ua", `"(Not(A:Brand";v="8", "Chromium";v="100"`)
-	req.Header.Set("Accept", "*/*")
-	req.Header.Set("X-Requested-With", "XMLHttpRequest")
-	req.Header.Set("sec-ch-ua-mobile", "?0")
-	req.Header.Set("User-Agent", `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36`)
-	req.Header.Set("sec-ch-ua-platform", `"macOS"`)
-	req.Header.Set("Origin", "http://"+ipAddress+":7396")
-	req.Header.Set("Sec-Fetch-Site", "same-origin")
-	req.Header.Set("Sec-Fetch-Mode", "cors")
-	req.Header.Set("Sec-Fetch-Dest", "empty")
-	req.Header.Set("Referer", "http://"+ipAddress+":7396/?nocache=0.4642702738328919")
-	req.Header.Set("Accept-Encoding", "gzip, deflate")
-	req.Header.Set("Accept-Language", "en-GB,en-US;q=0.9,en;q=0.8")
-	req.Header.Set("Connection", "close")
-
-	// Send the HTTP request
-	client := &http.Client{}
-	resp, err := client.Do(req)
+	var responses []Slot
+	err = json.Unmarshal([]byte(body), &responses)
 	if err != nil {
-		fmt.Println("Error sending HTTP request:", err)
+		fmt.Println("Error unmarshalling JSON:", err)
+		return err, nil
 	}
-	defer resp.Body.Close()
 
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("Error reading response body:", err)
-	}
-	return string(body)
+	return nil, responses
 }
 
 func printProgressBar(percent float64) {
-	totalBars := 100
+	totalBars := 50
 	doneBars := int(percent / 2)
-	fmt.Printf("[")
+	fmt.Printf("\r[")
 	for i := 0; i < totalBars; i++ {
 		if i < doneBars {
 			fmt.Print("=")
@@ -236,35 +146,16 @@ func printProgressBar(percent float64) {
 	fmt.Printf("%.2f%%", percent)
 }
 
-func taskPercentage(data string) (error, float64) {
-	var responses [][]interface{}
-	err := json.Unmarshal([]byte(data), &responses)
-	if err != nil {
-		fmt.Println("Error unmarshalling JSON:", err)
-		return err, 0
+func taskPercentage(data []Slot) (error, float64) {
+	for _, slot := range data {
+		percentStr := strings.TrimRight(slot.PercentDone, "%")
+		percent, err := strconv.ParseFloat(percentStr, 64)
+		if err != nil {
+			fmt.Println("Error parsing percent:", err)
+			return err, 0
+		}
+		return nil, percent
 	}
-
-	//for _, response := range responses {
-	//	//responseObject := response.(Response)
-	//	if response == "/api/slots" {
-	//		var slots []Slot
-	//		err = json.Unmarshal([]byte(fmt.Sprintf("%v", responseObject.Value)), &slots)
-	//		if err != nil {
-	//			fmt.Println("Error unmarshalling slots:", err)
-	//			return err, 0
-	//		}
-	//
-	//		for _, slot := range slots {
-	//			percentStr := strings.TrimRight(slot.PercentDone, "%")
-	//			percent, err := strconv.ParseFloat(percentStr, 64)
-	//			if err != nil {
-	//				fmt.Println("Error parsing percent:", err)
-	//				return err, 0
-	//			}
-	//			return nil, percent
-	//		}
-	//	}
-	//}
 
 	return nil, 0
 }
@@ -276,7 +167,17 @@ func main() {
 	}
 	fahConfigured("192.168.0.20", sessionToken)
 	fahSetApi("192.168.0.20", sessionToken)
-	statusData := getStatus("192.168.0.20", sessionToken)
-	err, percentage := taskPercentage(statusData)
-	printProgressBar(percentage)
+	for {
+		err, statusData := getStatus("192.168.0.20", sessionToken)
+		if err != nil {
+			panic(err)
+		}
+		err, percentage := taskPercentage(statusData)
+		if err != nil {
+			panic(err)
+		}
+		printProgressBar(percentage)
+
+		time.Sleep(1)
+	}
 }
